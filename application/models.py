a4acd6a228d0cede2bf835e0cb0e587f56c6de94
Rodolpho Nascimento <rodolpho@essencialcode.com.br>
2023-01-12 17:03:20 -0300

    Finishing CRUD operations methods

--- application/models.py
+++ application/models.py
@@ -3,13 +3,23 @@ from flask_sqlalchemy import SQLAlchemy
 
 db = SQLAlchemy()
 
- 
+
 class Providers(db.Model):
    id = db.Column('id', db.String(16), primary_key=True)
    name = db.Column(db.String(100))
    company = db.Column(db.String(50))  
    created_at = db.Column(db.DateTime, default=db.func.current_timestamp())
    amount_products = db.Column(db.String(10))
+   
+   @classmethod
+   def serialize(self, provider):
+      return {
+         "name": provider.name,
+         "id": provider.id,
+         "company": provider.company,
+         "created_at": provider.created_at,
+         "amount_products": provider.amount_products
+      }
    
    
 def get_database():
--- application/providers/views.py
+++ application/providers/views.py
@@ -1,5 +1,5 @@
 from . import providers
-from flask import request, make_response, session
+from flask import request, make_response
 from application.models import Providers, get_database
 import json
 
@@ -14,34 +14,76 @@ def insert():
     try:
         provider = Providers()
         provider.id = data.get("id", "")
-        provider.name = data.get("name", "")
-        provider.company = data.get("company", "")
-        provider.amount_products = data.get("amount_products", "")
+        provider.name = data.get("name", "not informed")
+        provider.company = data.get("company", "not informed")
+        provider.amount_products = data.get("amount_products", 1)
         
         db = get_database()
         db.session.add(provider)
         db.session.commit()
-        response = make_response("", 200)
+        return make_response("", 201)
         
     except Exception as e:
-        response = make_response(e, 401)
+        return make_response(str(e), 400)
         
-    return response
+        
+
+@providers.route('/api/providers/update/<id>', methods=["PUT"])
+def update(id):
+    data = request.data.decode("utf-8")
+    data = json.loads(data)
+    
+    try:
+        provider = Providers.query.filter_by(id=id).first()
+        if provider == None:
+            return make_response('', 404)
+        
+        provider.name = data.get("name", provider.name )
+        provider.company = data.get("company", provider.company)
+        provider.amount_products = data.get("amount_products", provider.amount_products)
+        
+        db = get_database()
+        db.session.commit()
+        output =  Providers.serialize(provider)
+        return make_response(output, 200)
+        
+    except Exception as e:
+        return make_response(str(e), 400)
+    
 
 
-@providers.route('/api/providers/update/', methods=["PUT"])
-def update():
-    request.data
-    pass
-
-
+@providers.route('/api/providers/list/<id>', methods=["GET"])
 @providers.route('/api/providers/list/', methods=["GET"])
-def list():
-    request.data
-    pass
+def list(id=None):
+    try:
+        db = get_database()
+        if id != None:
+            providers = db.session.execute(db.select(Providers).filter_by(id=id).order_by(Providers.name)).scalars()
+        else:
+            providers = db.session.execute(db.select(Providers).order_by(Providers.name)).scalars()
+        
+        output = [Providers.serialize(provider) for provider in providers]
+        if len(output) == 0:
+            return make_response(output, 404)
+        else:
+            return make_response(output, 200)
+        
+    except Exception as e:
+        return make_response(str(e), 400)        
 
 
-@providers.route('/api/providers/delete/', methods=["DELETE"])
-def delete():
-    request.data
-    pass
+
+@providers.route('/api/providers/delete/<id>', methods=["DELETE"])
+def delete(id):
+    try:
+        provider = Providers.query.filter_by(id=id).first()
+        if provider == None:
+            return make_response('', 404)        
+       
+        db = get_database()
+        db.session.delete(provider)
+        db.session.commit()
+        return make_response("", 200)
+        
+    except Exception as e:
+        return make_response(str(e), 400)
